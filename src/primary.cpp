a575ae881178c840ed9e0e3a1e5ee40a796822ec
Photon Law <photonlaw6+git@gmail.com>
2024-08-12 16:28:40 -0400

    Added stylization to startup blurb

--- src/primary.cpp
+++ src/primary.cpp
@@ -5,14 +5,18 @@
 void GAME_INIT(bool silent)
 {
 	if(!silent){
+		std::cout << "\x1b[32;1m";
 		std::cout << ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;" << std::endl;
 		std::cout << ";;        SCP: Green Insignia       ;;" << std::endl;
 		std::cout << ";;            By Photon Law         ;;" << std::endl;
 		std::cout << ";;  A game set in the SCP Universe  ;;" << std::endl;
 		std::cout << ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;" << std::endl;
 		SDL_Delay(3000);
+		std::cout << "\x1b[0m";
+		std::cout << "\x1b[31;3m";
 		std::cout << "The following are on the SCP Wiki, under CC-BY-SA liscence:" << std::endl;
 		SDL_Delay(2500);
+		std::cout << "\x1b[23m";
 		std::cout << std::endl;
 		std::cout << "Chaos Insurgency written by Kain Pathos Crow" << std::endl;
 		SDL_Delay(500);
@@ -27,6 +31,7 @@ void GAME_INIT(bool silent)
 		std::cout << "SCP 106 written by Dr Gears" << std::endl;
 		std::cout << "=====================================================" << std::endl;
 		SDL_Delay(5000);
+		std::cout << "\x1b[34m";
 		std::cout << "Programming by Photon Law" << std::endl;
 		SDL_Delay(200);
 		std::cout << "Textures by Photon Law" << std::endl;
@@ -44,10 +49,12 @@ void GAME_INIT(bool silent)
 		std::cout << "Business & Legal contact: photonlaw6+SCPGI@gmail.com" << std::endl;
 		std::cout << "=====================================================" << std::endl;
 		SDL_Delay(1000);
+		std::cout << "\x1b[37;2m";
 		std::cout << "SCP: " << WIN_TITLE << " is distributed under the CC-BY-SA liscence" << std::endl;
 		std::cout << "https://creativecommons.org/licenses/by-sa/3.0/deed.en" << std::endl;
 		SDL_Delay(3000);
 		std::cout << "=====================================================" << std::endl;
+		std::cout << "\x1b[0m" << std::endl;
 	}
 }
 
